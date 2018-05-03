249c9f7b354a254a766030a243d4529ca24fcb2e
m305-6\w401 <a106082027@mail.shu.edu.tw>
2018-05-03 18:06:01 +0800

    1000分

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -31,8 +31,20 @@ namespace WpfApplication1
             HeightNumber.Text = heightValue.ToString();
             double weightValue = Math.Round(WeightSlider.Value, 1);
             WeightNumber.Text = weightValue.ToString();
+            // position
             Canvas.SetLeft(Height, heightValue * 1.6);
             Canvas.SetLeft(Weight, weightValue * 1.6);
+            // bmi
+            double bmi;
+            bmi = weightValue /Math.Pow((heightValue / 100), 2) ;
+
+            // split
+            string[] parts = Math.Round(bmi, 1).ToString().Split('.');
+            intBmi.Text = parts[0];
+            if (parts.Length > 1)
+                dotBmi.Text = "." + parts[1];
+            else
+                dotBmi.Text = ".0";
         }
     }
 }
